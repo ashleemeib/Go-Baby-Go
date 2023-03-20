cb336dcddcbc9658c99a58576a41edd135f12d4b
LadyTartarus <117467426+LadyTartarus@users.noreply.github.com>
2023-03-19 23:56:27 -0700

    Add files via upload

--- AdSensorMaster3.17vel.ino
+++ AdSensorMaster3.17vel.ino
@@ -17,8 +17,8 @@
 RV8803 rtc;
 SparkFun_KX134 kxAccel;
 outputData myData;   // Struct for the accelerometer's data
-float  zO = 0.0;
-float zOx = 0.0;
+float  zO = 0.0;//zero g offset
+float zOx = 0.0; // x direction offset
 //float zOy = 0.0;
 //float zOz = 0.0;
 float lastVel = 0.0;
@@ -29,32 +29,13 @@ float lastAccel = 0.0;
 int det = 0;
 int det1 = 0;
 int timet = 100;
-int sampRate = 50;
 bool detM = false;
 bool detS = false;
 bool set = false;
 
 //Pins
 #define EVI 13
-//Accelerometer subroutine:(Debug:serial prints the readings)
-void Accel()
-{
-  // Check if data is ready.
 
-
-  Serial.print("X: ");
-  Serial.print(myData.xData * 9.8, 4);
-  Serial.print(" Y: ");
-  Serial.print(myData.yData * 9.8, 4);
-  Serial.print(" Z: ");
-  Serial.print(myData.xData * 9.8, 4);
-  Serial.println();
-
-  //debug(may comment out delay)
-  delay(20); // Delay should be 1/ODR (Output Data Rate), default is 1/50ODR
-
-
-}
 //timestamp subroutine
 void RTC()
 {
@@ -76,6 +57,23 @@ void RTC()
 //reset function
 void(* resetFunc) (void) = 0; //declare reset function @ address 0
 //initialize
+void setFunc(){
+ while (timet != 0) {
+        Serial.print("Z1: ");
+        Serial.print(myData.xData * 9.80665, 4);
+        Serial.println();
+
+        zOx = (myData.xData * 9.80665) + zOx;
+        timet --;
+      }
+      Serial.print("SET");
+      timet = 100;
+      set = true;
+      zO = (zOx) / 100;
+      Serial.print(zO);
+    }
+  
+}
 void setup()
 {
   pinMode(EVI, OUTPUT);
@@ -113,77 +111,65 @@ void loop()
 {
 
   // Check if data is ready.
-
   if ( kxAccel.dataReady() )
   {
     kxAccel.getAccelData(&myData);
     if ( set == false) {
-      while (timet != 0) {
-        Serial.print("Z1: ");
-        Serial.print(myData.xData * 9.80665, 4);
-        Serial.println();
-
-        zOx = (myData.xData * 9.80665) + zOx;
-        timet --;
-      }
-      Serial.print("SET");
-      timet = 100;
-      set = true;
-      zO = (zOx) / 100;
-      Serial.print(zO);
+     setFunc();//calculates the zerog offset
     }
   } else {
     //resetFunc();
   }
+  
   //constants calculations
   accel = (myData.xData * 9.80665) - zO;
-  vel =  lastVel + (accel - lastAccel) * (1 / 10);
-  curVel = curVel + vel;
+  vel =  /*lastVel +*/ ((accel - lastAccel)*0.1);//Possibly needs reworking
+  curVel = curVel + vel;//current velocity update
   // detection from idle
-  if (curVel >= 0.1 && detM == false) {
+  if (abs(curVel) >= 0.05 && detM == false) {//absolute value to account for positive and negative change
     Serial.print("V:");
     Serial.print(curVel, 4);
     Serial.println();
     Serial.print(" A: ");
     Serial.print(accel, 4);
     Serial.println();
-    digitalWrite(EVI, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(EVI, HIGH);   // trigger EVI pin
     delay(20);                       // wait for a second
-    digitalWrite(EVI, LOW);    // turn the LED off by making the voltage LOW
-    Serial.print("Detected ");
+    digitalWrite(EVI, LOW);    // turn off EVI pin output
+    Serial.print("Detected ");//Debug purposes
     RTC();//timestamp
-    //print to SD
-    // det1 = 0;//reset
+    //insert print to SD code here
     detM = true;
-  } else if (curVel < 0.1 && detM == true) {
+  } else if (abs(curVel) < 0.04 && detM == true) {//sto detection, parameters need testing
     Serial.print(" V: ");
     Serial.print(curVel, 4);
     Serial.println();
     Serial.print(" A: ");
     Serial.print(accel, 4);
     Serial.println();
-    digitalWrite(EVI, HIGH);   // turn the LED on (HIGH is the voltage level)
+    digitalWrite(EVI, HIGH);   // trigger EVI pin
     delay(20);                       // wait for a second
-    digitalWrite(EVI, LOW);    // turn the LED off by making the voltage LOW
+    digitalWrite(EVI, LOW);    // turn off EVI pin output
     Serial.print("Stopped");
     RTC();
     detM = false;
   } else {
-    Serial.print(" lV: ");
-    Serial.print(lastVel, 8);
+    Serial.print(" cV: ");
+    Serial.print(curVel, 8);
     Serial.print(" V: ");
-    Serial.print(vel, 8);
+    //Serial.print(vel, 8);
     Serial.println();
-    Serial.print(" A: ");
-    Serial.print((accel), 4);
+    Serial.print("L A: ");
+   // Serial.print((accel), 4);
     Serial.print(" lA: ");
-    Serial.print((lastAccel), 4);
+    //Serial.print((lastAccel), 4);
     Serial.println();
   }
+  //sets last values
   lastVel = vel;
   lastAccel = accel;
 
-  delay(100); // Delay should be 1/ODR (Output Data Rate), default is 1/50ODR(adjusted)
+  delay(100); // (Delay should be 1/ODR (Output Data Rate), default was 1/50ODR(adjusted)50hz) now is 10Hz
 
 
 
